9c2ec27e01082a579b3984bd09fd2b56f87b27a4
下田　佳紀 <y.s@platinum.local>
2020-12-22 17:23:33 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Nginx
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators.system_tests = nil
+  end
+end
